7470d5d1654fd343e406e66d2643ef963d4a7c4e
Prasanjit dutta <57594105+prasanjit101@users.noreply.github.com>
2021-01-14 13:52:26 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,9 +4,4 @@ const app = Firebase.initializeApp({
 	//your firebase api
 });
 
-export const db = app.database();
-export const ref= db.ref('Admin');
-export const usersref= db.ref('Users');
-export const contactsref= db.ref('Admin/Contacts');
-export const dataref= db.ref("Admin/Data Collection/");
-export const pgref= db.ref("Admin/Data Collection/Paying Guest/Nagaon/Single");
+//create exports
